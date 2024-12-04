a926cbe2845ac3db8dd0c94d793a7510eededbdc
kaishajri01 <102411715+kaishajri01@users.noreply.github.com>
2024-12-04 18:21:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from setuptools import setup, find_packages
 
 setup(
@@ -14,20 +13,3 @@ setup(
     ],
     python_requires='>=3.7',
 )
-=======
-from setuptools import setup, find_packages
-
-setup(
-    name='datalib',
-    version='1.0.0',
-    description='A Python library for data manipulation and analysis',
-    author='Kais Hajri',
-    author_email='kais.hajri01@gmail.com',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    install_requires=[
-        'pandas', 'numpy', 'matplotlib', 'scikit-learn', 'seaborn'
-    ],
-    python_requires='>=3.7',
-)
->>>>>>> origin/main
